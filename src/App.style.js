8f2ff4af3248eb86a0da6efe8f3831517af1e03b
sergio rodriguez <sergrodri92@gmail.com>
2017-07-11 23:36:39 -0500

    Basic UI set up

---
+++ src/App.style.js
@@ -0,0 +1,14 @@
+// import styled from 'styled-components';
+//
+// export const UIContainer = styled.div`
+//   background-color: #454545 ;
+//   color: black;
+//   text-align: center;
+//
+//   height: 600px;
+//   width: 400px;
+//
+//   margin: 0 auto;
+//   margin-top: 100px;
+//   border: 2px solid black;
+//   `;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background-color: #F5F5F5;
+  }
+`
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
